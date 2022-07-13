b296ff812335be87128e1da36d28c907de25795f
Prashant P. Khairnar <36958282+prashantkh70@users.noreply.github.com>
2022-07-13 23:49:15 +0530

    Add files via upload

---
+++ # classes.py
@@ -0,0 +1,28 @@
+# classes
+# constructors, attributes , methods
+
+# classes are real world entity or object
+
+class Car:
+    pass
+
+car1= Car()
+car2= Car()
+
+print(car1)
+
+car1.windows =4
+car1.tyres = 4
+car1.engine= "diesel"
+
+car2.windows=6
+car2.tyres=6
+car2.engine="petrol"
+
+#print(car1.engine)
+#print(car2.engine)
+
+print(dir(car1))
+print(dir(car2))
+
+
